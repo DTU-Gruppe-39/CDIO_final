ec76b5723182b64327bee691cd97d65a6d156dd8
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-01-04 15:22:16 +0100

    Made getProperty value
    Dont know if it works tho.

--- MavenProject/src/entity/Player.java
+++ MavenProject/src/entity/Player.java
@@ -1,22 +1,27 @@
 package entity;
 
+import gamelogic.Game;
+
 public class Player {
 	private String name;
-//	private TwoDice dice;
+	//	private TwoDice dice;
 	private AccountBalance account;
 	private boolean isDead;
 	private boolean isWinner;
 	private boolean isJailed;
 	private int currentField = 0;
+	private int netWorth;
+	private int propertyValue;
 
-	
+
 	public Player () {
-//		TwoDice dice = new TwoDice();
+		//		TwoDice dice = new TwoDice();
 		this.account = new AccountBalance(0);
-//		this.dice = dice;
+		//		this.dice = dice;
 		isDead = false;
 		isWinner = false;
 		isJailed = false;
+		netWorth = 0;
 	}
 
 	public int getCurrentField() {
@@ -27,9 +32,9 @@ public class Player {
 		this.currentField = currentField;
 	}
 
-//	public TwoDice getDice() {
-//		return dice;
-//	}
+	//	public TwoDice getDice() {
+	//		return dice;
+	//	}
 
 	public int getBalance() {
 		return this.account.balance;
@@ -54,15 +59,15 @@ public class Player {
 	public boolean isDead() {
 		return isDead;
 	}
-	
+
 	public void setDead(boolean isDead) {
 		this.isDead = isDead;
 	}
-	
+
 	public boolean isJailed() {
 		return isJailed;
 	}
-	
+
 	public void setJailed(boolean isJailed) {
 		this.isJailed = isJailed;
 	}
@@ -73,5 +78,36 @@ public class Player {
 
 	public void setWinner(boolean Winner) {
 		this.isWinner = Winner;
+	}
+
+	public int getNetWorth() {
+		netWorth = getBalance() + getPropertyValue() ;
+		return netWorth;
+	}
+	public int getPropertyValue() { //method that gets a players property value
+		for (int i=0; i<40; i++) {
+			
+		
+		
+		}
+
+
+
+
+
+
+			//		for (int i=0; i<40; i++) {
+			//			//			if(ListOfPlayers.getPlayers(whosTurn).getBalance()<=(Fields[ListOfPlayers.getPlayers(whosTurn).getCurrentField()][1]))
+			//			if (whosTurn==Fields[i][4]) {
+			//				Fields[i][4]=0;
+			//				Fields[i][3]=0;
+			//				ListOfPlayers.getPlayers(whosTurn).setNewBalance(Fields[i][1]);
+			//				removeOwner(i);
+			//
+							return propertyValue;
+			//			}
+			//		}
+			//	}
+		
 	}
 }
