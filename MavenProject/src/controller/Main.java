8a624aa5b881d2918e452ea130df23e8310e0e9b
rallerG <raller799@live.dk>
2018-01-05 11:43:35 +0100

    Call read subtext

--- MavenProject/src/controller/Main.java
+++ MavenProject/src/controller/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 //		new GameController().playGame();
 		GUI_GUI.readDisc();
+		GUI_GUI.readSubText();
 		GUI_GUI.GUILauncher();
 		Game.fillFields();
 		ListOfPlayers list = new ListOfPlayers((GUI_GUI.getNumberOfPlayers() + 1)); //Players index in array needs to start at 1, because 0 = to no owner on a field
