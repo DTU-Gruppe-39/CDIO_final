4572187f7373187b3d6b2b3bfc283c25db6b710b
Cilian <32563603+Cilian@users.noreply.github.com>
2018-01-10 11:20:41 +0100

    Fixed paycard

--- MavenProject/src/controller/Main.java
+++ MavenProject/src/controller/Main.java
@@ -7,14 +7,12 @@ import entity.GameBoard;
 public class Main {
 	public static void main(String[] args) throws IOException {
 
-		ChanceDeck.readAmaount();
-
 //		new GameController().playGame();
 		GUI_GUI.readDisc();
 		GUI_GUI.readSubText();
 		GUI_GUI.GUILauncher();
 		ChanceDeck.CreateCards();
-		ChanceDeck.shuffle();
+//		ChanceDeck.shuffle();
 		GameBoard.fillFields();
 		GameBoard.adjustPrices();
 		ListOfPlayers list = new ListOfPlayers((GUI_GUI.getNumberOfPlayers() + 1)); //Players index in array needs to start at 1, because 0 = to no owner on a field
