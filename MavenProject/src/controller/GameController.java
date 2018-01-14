ccdffb956561604cba79c9e5748adf904a3ad379
Patrick Marcell Madsen <s175209@student.dtu.dk>
2018-01-14 21:22:29 +0100

    Added GameController and cleaned up main method

---
+++ MavenProject/src/controller/GameController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import boundary.GUI_GUI;
+import entity.GameBoard;
+import entity.HousePrice;
+import gamelogic.Game;
+
+public class GameController {
+
+	public void playGame() throws IOException{
+		HousePrice.readHousePrice();
+		HousePrice.setupHouse();
+		System.out.println(Arrays.toString(HousePrice.getHousePriceInt()));
+		GUI_GUI.readDisc();
+		GUI_GUI.readSubText();
+		GUI_GUI.GUILauncher();
+		GameBoard.fillFields();
+		ListOfPlayers list = new ListOfPlayers((GUI_GUI.getNumberOfPlayers() + 1)); //Players index in array needs to start at 1, because 0 = to no owner on a field
+		list.addplayer(GUI_GUI.getNumberOfPlayers());		
+		Game.gameLogic();
+	}
+}
